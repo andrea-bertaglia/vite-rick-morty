cc7ddda6b7e1480d2286d45ff774ed6b1a5e39eb
andrea-bertaglia <andreabertaglia@gmail.com>
2024-05-06 16:25:07 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+
+    }
+  },
+  created() {
+    axios
+      .get('https://rickandmortyapi.com/api/character')
+      .then((resp) => {
+        console.log(resp);
+      });
+  },
+}
+</script>
+
+<template>
+  <h1>CIAO</h1>
+</template>
+
+<style lang="scss"></style>
