b61b42fc4052c83d5af5aeedfafaaac268bcee13
andrea-bertaglia <andreabertaglia@gmail.com>
2024-05-06 16:29:27 +0200

    feat: API call and save result in array

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,17 @@ import axios from 'axios';
 export default {
   data() {
     return {
-
+      cardsArray: []
     }
   },
   created() {
     axios
       .get('https://rickandmortyapi.com/api/character')
       .then((resp) => {
-        console.log(resp);
+        // console.log(resp);
+        // salvo l'oggetto della chiamata API nell'array
+        this.cardsArray = resp.data.results;
+        console.log(this.cardsArray);
       });
   },
 }
