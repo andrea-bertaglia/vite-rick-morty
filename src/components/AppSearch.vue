2f6608863bead66150d27808daedaaed5e975635
andrea-bertaglia <andreabertaglia@gmail.com>
2024-05-07 19:36:15 +0200

    feat: CounterElements component (number of items found)

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -1,28 +1,40 @@
 <script>
 import { store } from "../store"
+import CounterElements from '../components/CounterElements.vue';
 export default {
+    components: {
+        CounterElements,
+    },
     data() {
         return {
             store,
             statusArray: ["All", "Alive", "Dead", "Unknown"]
         }
-    }
+    },
+
 }
 </script>
 
 <template>
     <div class="container py-4">
-        <div class="row text-center">
-            <div>
-                <span class="px-2">Seleziona lo status</span>
-                <select @change="$emit('filter')" v-model="store.selectedStatus">
-                    <option :value="status" v-for="status in statusArray">
-                        {{ status }}</option>
-                </select>
+        <div class="row">
+
+            <div class="d-flex justify-content-between">
+                <div>
+                    <span class="px-2">Seleziona lo status</span>
+                    <select @change="$emit('filter')" v-model="store.selectedStatus">
+                        <option :value="status" v-for="status in statusArray">
+                            {{ status }}</option>
+                    </select>
+
+                </div>
+
+                <CounterElements :cardObj="this.store.cardsList" />
+
+
             </div>
-            <div>
-                <!-- <p>Trovati {{ store.CardsList.length }} elementi</p> -->
-            </div>
+
+
         </div>
     </div>
 </template>
---
+++ src/components/CounterElements.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    props: {
+        // passo l'oggetto da CardsList.vue
+        cardObj: Object,
+    },
+}
+</script>
+
+<template>
+    <span>Trovati <strong class="text-decoration-underline">{{ cardObj.length }}</strong>
+        elementi</span>
+</template>
